8b0f2ed88b30b6c418172df5421772754ba4e6ab
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2022-08-04 00:59:05 +0530

    Time: 291 ms (5.03%), Space: 16.9 MB (8.22%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int n=matrix.size();
-        priority_queue<int> pq;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(pq.size()<k+1){
-                    pq.push(matrix[i][j]);
-                }
-                else{
-                    if(pq.top()>matrix[i][j]){
-                        pq.pop();
-                        pq.push(matrix[i][j]);
-                    }
-                }
-            }
+        // METHOD 1: Using MAX-HEAP
+        // int n=matrix.size();
+        // priority_queue<int> pq;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(pq.size()<k+1){
+        //             pq.push(matrix[i][j]);
+        //         }
+        //         else{
+        //             if(pq.top()>matrix[i][j]){
+        //                 pq.pop();
+        //                 pq.push(matrix[i][j]);
+        //             }
+        //         }
+        //     }
+        // }
+        // if(pq.size()>k)
+        //     pq.pop();
+        // return pq.top();
+        
+        
+        //METHOD 2:Using MIN-HEAP
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        int count=0,m=matrix.size(),n=matrix[0].size();
+        for(int i=0;i<min(k,m);i++){
+            pq.push({matrix[i][0],i,0});
         }
-        if(pq.size()>k)
-            pq.pop();
-        return pq.top();
+        auto it=pq.top();
+        while(count<k-1){
+            if(!pq.empty()){
+            it=pq.top();
+                pq.pop();
+            }           
+            if(it[2]+1<n){
+                pq.push({matrix[it[1]][it[2]+1],it[1],it[2]+1});
+            }
+            count++;
+        }
+        if(!pq.empty())return pq.top()[0];
+        return 0;
     }
 };
