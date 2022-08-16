96bf88ac4b27d6024a9ca733f32d35077ba2e0c5
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2022-08-17 02:13:32 +0530

    Time: 40 ms (10.34%), Space: 10 MB (5.01%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
-int romanToInt(string s) 
-{
-    unordered_map<char, int> T = { { 'I' , 1 },
-                                   { 'V' , 5 },
-                                   { 'X' , 10 },
-                                   { 'L' , 50 },
-                                   { 'C' , 100 },
-                                   { 'D' , 500 },
-                                   { 'M' , 1000 } };
-                                   
-   int sum = T[s.back()];
-   for (int i = s.length() - 2; i >= 0; --i) 
-   {
-       if (T[s[i]] < T[s[i + 1]])
-       {
-           sum -= T[s[i]];
-       }
-       else
-       {
-           sum += T[s[i]];
-       }
-   }
-   
-   return sum;
-}
+    int romanToInt(string s) {
+        unordered_map<char,int> mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        unordered_map<char,char> subtract;
+        subtract['V']='I';
+        subtract['X']='I';
+        subtract['L']='X';
+        subtract['C']='X';
+        subtract['D']='C';
+        subtract['M']='C';
+        if(s.length()==1)
+            return mp[s[0]];
+        int value=0,i=0;
+        for(i=0;i<s.length()-1;i++){
+            auto it=subtract.find(s[i+1]);
+            if(it!=subtract.end()&&it->second==s[i]){
+                value-=mp[s[i]];
+            }
+            else{
+                value+=mp[s[i]];
+            }
+            // if(it==subtract.end()){
+            //     value+=mp[s[i]];
+            // }
+            // else{
+            //     value-=mp[s[i]];
+            // }
+        }
+        value+=mp[s[i]];
+        return value;
+    }
 };
