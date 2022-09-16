bc1cb21a26b87fda4a6f6ff3727cda6063cfb64f
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2022-09-17 04:23:54 +0530

    Time: 807 ms (33.86%), Space: 51.9 MB (99.06%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -16,13 +16,14 @@ public:
         dp.clear();
         // dp.resize(nums.size(),vector<int>(multipliers.size(),-1));
         // return fun(nums,multipliers,0,nums.size()-1,0);
-        dp.resize(multipliers.size()+1,vector<int>(multipliers.size()+1,0));
+        // dp.resize(multipliers.size()+1,vector<int>(multipliers.size()+1,0));
+        dp.resize(multipliers.size()+1,vector<int>(3,0));
         for(int k=multipliers.size()-1;k>=0;k--){
             for(int i=k;i>=0;i--){
                 int a=nums[i]*multipliers[k];
                 int b=nums[nums.size()-1-k+i]*multipliers[k];
                 // if(i+1<multipliers.size())
-                    dp[i][k]=max(a+dp[i+1][k+1],b+dp[i][k+1]);
+                    dp[i][k%2]=max(a+dp[i+1][(k+1)%2],b+dp[i][(k+1)%2]);
             }
         }
         return dp[0][0];
