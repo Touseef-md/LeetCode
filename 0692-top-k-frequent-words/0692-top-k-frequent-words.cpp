9d402776b3ccb45e259bea40b9be3f31d99ee002
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2022-10-19 22:16:00 +0530

    Time: 16 ms (81.90%), Space: 12.8 MB (39.56%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,3 +1,4 @@
+#include<bits/stdc++.h>
 bool comp(pair<int,string>& a,pair<int,string> & b){
     if(b.first>a.first)
         return false;
@@ -8,20 +9,35 @@ bool comp(pair<int,string>& a,pair<int,string> & b){
     }
     return true;
 }
+// bool pqcomp(pair<int,string> &a,pair)
+// greater<pair<int,string>>
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        priority_queue<pair<int,string>,vector<pair<int,string>>,greater<pair<int,string>>> pq;
+        auto compp=[](pair<int,string> a,pair<int,string> b){
+    if(b.first>a.first)
+        return false;
+    else if(b.first==a.first){
+        if(b.second<=a.second)
+            return false;
+        return true;
+    }
+    return true;
+        };
+        priority_queue<pair<int,string>,vector<pair<int,string>>,decltype(compp)> pq(compp);
         unordered_map<string,int> mp;
         for(auto i:words){
             mp[i]++;
         }
         for(auto i:mp){
+            cout<<i.second<<"  "<<i.first<<endl;
             pq.push({i.second,i.first});
-            // if(pq.size()==k+1){
-            //     pq.pop();
-            // }
+            if(pq.size()==k+1){
+                pq.pop();
+            }
         }
+        // if(pq.size()==k+1)
+        //     pq.pop();
         vector<pair<int,string>> temp_result;
         while(!pq.empty()){
             temp_result.push_back(pq.top());
