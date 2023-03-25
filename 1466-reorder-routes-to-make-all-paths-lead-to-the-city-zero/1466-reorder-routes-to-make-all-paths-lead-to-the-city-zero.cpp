247af5699510ae1088cb02ee486870734f54d085
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2023-03-25 15:38:47 +0530

    Time: 419 ms (59.31%), Space: 118.7 MB (39.40%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,85 +1,32 @@
 class Solution {
 public:
-    vector<vector<bool>> reach;
-    vector<vector<int>> adj;
-    vector<vector<int>> revadj;
-    
     int count=0;
-    // void dfs1(int i){
-    //     for(auto it:adj[i]){
-    //         count++;
-    //         reach[it][0]=true;
-    //         dfs1(it);
-    //     }
-    // }
-    // void dfs(int i){
-    //     for(auto it:adj[i]){
-    //         if(!reach[it][0])
-    //     }
-    // }
-    int minReorder(int n, vector<vector<int>>& connections) {
-        reach.clear();
-        adj.clear();
-        revadj.clear();
-        adj.resize(n);
-        revadj.resize(n);
-        count=0;
-        reach.resize(n,vector<bool> (1,false));
-        reach[0][0]=true; priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        for(auto it:connections){
-            adj[it[0]].push_back(it[1]);
-            revadj[it[1]].push_back(it[0]);
-            if(it[1]==0){
-                reach[it[0]][0]=true;
-                pq.push({0,it[0]});
+    int dfs(vector<vector<pair<int,int>>>& adj_list,vector<int>& visit,int i){
+        int count=0;
+        // cout<<"Check "<<i<<endl;
+        visit[i]=1;
+        for(auto j:adj_list[i]){
+            if(visit[j.first]==1)
+                continue;
+            if(j.second==1){
+                count+=1;
+                count+=dfs(adj_list,visit,j.first);
+            }
+            else{
+                count+=dfs(adj_list,visit,j.first);
             }
         }
-        pq.push({0,0});
-        while(!pq.empty()){
-            int r=pq.top().first;
-            int i=pq.top().second;
-            pq.pop();
-            for(auto it:adj[i]){
-                if(!reach[it][0]){
-                    count++;
-                    reach[it][0]=true;
-                    pq.push({0,it});
-                }
-            }
-            for(auto it:revadj[i]){
-                if(!reach[it][0]){
-                    reach[it][0]=true;
-                    pq.push({0,it});
-                }
-            }
-        }
-        // dfs(0);
-        // for(int i=0;i<n;i++){
-        //     for(auto it:connections){
-        //         if(it[0]==0){
-        //             if(!reach[it[1]][it[0]]){
-        //                 count++;
-        //                 reach[it[1]][it[0]]=true;
-        //             }
-        //         }
-        //         else if(it[1]==0){
-        //             if(!reach[it[0]][0]){
-        //                 // count++;
-        //                 reach[it[0]][0]=true;
-        //             }
-        //         }
-        //         else{
-        //             if(reach[it[0]][0]){
-        //                 if(!reach[it[1]][0]){
-        //                     count++;
-        //                     reach[it[1]][0]=true;
-        //                 }
-        //             }
-        //             else if(reach[it[1]][0])
-        //                 reach[it[0]][0]=true;
-        //         }
-        //     }
-        // }
         return count;
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<pair<int,int>>> adj_list(n);
+        for(auto i:connections){
+            adj_list[i[0]].push_back({i[1],1});
+            adj_list[i[1]].push_back({i[0],0});
+        }
+        vector<int> visit(n,0);
+        count=0;
+         return dfs(adj_list,visit,0);
+        
     }
 };
