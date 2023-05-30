b0806098ace30febcd52e49342e65ff872297aad
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2023-05-30 02:10:15 -0400

    Time: 104 ms (57.90%), Space: 41.4 MB (68.08%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+public:
+    unordered_map<int,int> mp;
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        mp[key]++;
+    }
+    
+    void remove(int key) {
+        mp.erase(key);
+    }
+    
+    bool contains(int key) {
+        auto it=mp.find(key);
+        if(it!=mp.end())
+            return true;
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
