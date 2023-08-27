49169b121906ead1a17a08ebc8508983333bb982
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2023-08-27 15:32:31 +0530

    Time: 156 ms (66.11%), Space: 22.9 MB (44.16%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -14,22 +14,40 @@ public:
             return true;
     }
     int findLongestChain(vector<vector<int>>& pairs) {
+        int result=1,ans=0,n=pairs.size(),count=1,end;
+        vector<int> dp(n+1,0);
         sort(pairs.begin(),pairs.end(),comp);
-        // if
-        int n=pairs.size(),count=1,end;
-        if(n==1){
-            return 1;
-        }
-        end=pairs[0][1];
-        for(int i=1;i<pairs.size();i++){
-            if(pairs[i][0]<=end){
-                // i++;
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            ans=0;
+            for(int j=0;j<i;j++){
+                if(pairs[j][1]<pairs[i][0]){
+                    // cout<<"This is it "<<i<<"  "<<j<<endl;
+                    
+                    ans=max(ans,dp[j]);
+                }
             }
-            else{
-                end=pairs[i][1];
-                count++;
-            }
+            dp[i]=max(dp[i],ans+1);
+            result=max(result,dp[i]);
         }
-        return count;
+        return result;
+        
+        
+        
+        // // if
+        // if(n==1){
+        //     return 1;
+        // }
+        // end=pairs[0][1];
+        // for(int i=1;i<pairs.size();i++){
+        //     if(pairs[i][0]<=end){
+        //         // i++;
+        //     }
+        //     else{
+        //         end=pairs[i][1];
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
 };
