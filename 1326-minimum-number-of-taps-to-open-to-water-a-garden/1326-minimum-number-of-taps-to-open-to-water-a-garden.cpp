df5782bedeffed263dd21c09008d87700482f72d
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2023-09-01 02:03:35 +0530

    Time: 30 ms (61.85%), Space: 14.6 MB (55.48%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -13,39 +13,54 @@ public:
         return true;
     }
     int minTaps(int n, vector<int>& ranges) {
-        vector<vector<int>> intervals;
-        for(int i=0;i<ranges.size();i++){
-            intervals.push_back({max(0,i-ranges[i]),min(n,i+ranges[i])});
+        // vector<vector<int>> intervals;
+        int INF=1000000;
+        vector<int> dp(n+2,INF);
+        dp[0]=0;
+        for(int i=0;i<=n;i++){
+            int start=max(0,i-ranges[i]);
+            int end=min(n,i+ranges[i]);
+            for(int j=start;j<=end;j++){
+                dp[end]=min(dp[end],dp[j]+1);
+            }
         }
-        sort(intervals.begin(),intervals.end(),comp);
-        if(intervals[0][0]!=0){
+        if(dp[n]==INF){
             return -1;
         }
-        int result=1,prevEnd=intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-            // if(prevEnd==n)return result;
-            if(intervals[i][1]<=prevEnd){
-                continue;
-            }
-            else{
-                if(intervals[i][0]>prevEnd){
-                    return -1;
-                }
-                else{
-                    int temp=0;
-                    while(i<intervals.size()&&intervals[i][0]<=prevEnd){
-                        temp=max(temp,intervals[i][1]);
-                        i++;
-                    }
-                    i--;
-                    prevEnd=temp;
-                    result++;
-                    // prevEnd=intervals[i][1];
-                }
-            }
-            // cout<<i<<endl;
+        return dp[n];
+        
+        // for(int i=0;i<ranges.size();i++){
+        //     intervals.push_back({max(0,i-ranges[i]),min(n,i+ranges[i])});
+        // }
+        // sort(intervals.begin(),intervals.end(),comp);
+//         if(intervals[0][0]!=0){
+//             return -1;
+//         }
+//         int result=1,prevEnd=intervals[0][1];
+//         for(int i=1;i<intervals.size();i++){
+//             // if(prevEnd==n)return result;
+//             if(intervals[i][1]<=prevEnd){
+//                 continue;
+//             }
+//             else{
+//                 if(intervals[i][0]>prevEnd){
+//                     return -1;
+//                 }
+//                 else{
+//                     int temp=0;
+//                     while(i<intervals.size()&&intervals[i][0]<=prevEnd){
+//                         temp=max(temp,intervals[i][1]);
+//                         i++;
+//                     }
+//                     i--;
+//                     prevEnd=temp;
+//                     result++;
+//                     // prevEnd=intervals[i][1];
+//                 }
+//             }
+//             // cout<<i<<endl;
            
-        }
-        return result;
+//         }
+//         return result;
     }
 };
