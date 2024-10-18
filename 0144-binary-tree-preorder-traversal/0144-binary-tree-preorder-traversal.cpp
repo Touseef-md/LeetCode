4ec0e40bf221f144f8a0aefac26669ac393288b4
Mohammad Touseef <79767012+Touseef-md@users.noreply.github.com>
2024-10-18 07:22:20 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (13.40%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -12,52 +12,77 @@
 class Solution {
 public:
     vector<int> result;
+    stack<TreeNode*> st;
     void preorder(TreeNode* root){
-        if(root==NULL)
+        if( root == NULL){
             return;
-        result.push_back(root->val);
-        preorder(root->left);
-        preorder(root->right);
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-       //------------------ Recursive Solution //
-        // if(root==NULL)
-        //     return {};
-        // result.clear();
-        // preorder(root);
-        // return result;
-        //----------------- Iterative Solution //---------
-            if(root==NULL)
-            return {};
-        result.clear();
-        queue<TreeNode*> left;
-        stack<TreeNode*> right;
-        left.push(root);
-        TreeNode* temp;
-        while(right.size()>0||left.size()>0){
-            if(left.size()>0){
-                temp=left.front();
-                left.pop();
-                result.push_back(temp->val);
-                if(temp->left!=NULL){
-                    left.push(temp->left);
-                }
-                if(temp->right!=NULL){
-                    right.push(temp->right);
-                }
-            }
-            else if(right.size()>0){
-                temp=right.top();
-                right.pop();
-                result.push_back(temp->val);
-                if(temp->left!=NULL){
-                    left.push(temp->left);
-                }
-                if(temp->right!=NULL){
-                    right.push(temp->right);
-                }
-            }
         }
+        if(root->left == NULL && root->right ==NULL){
+            result.push_back(root->val);
+            return;
+        }
+        result.push_back(root->val);
+        if( root->left != NULL ){
+            preorder(root->left);
+        }
+
+        if(root->right != NULL){
+            preorder(root->right);
+        }
+    }
+    vector<int> preorderTraversal(TreeNode* root){
+        result.clear();
+        preorder(root);
         return result;
+        
+        
+        
+        
+    //     if(root==NULL)
+    //         return;
+    //     result.push_back(root->val);
+    //     preorder(root->left);
+    //     preorder(root->right);
+    // }
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //    //------------------ Recursive Solution //
+    //     // if(root==NULL)
+    //     //     return {};
+    //     // result.clear();
+    //     // preorder(root);
+    //     // return result;
+    //     //----------------- Iterative Solution //---------
+    //         if(root==NULL)
+    //         return {};
+    //     result.clear();
+    //     queue<TreeNode*> left;
+    //     stack<TreeNode*> right;
+    //     left.push(root);
+    //     TreeNode* temp;
+    //     while(right.size()>0||left.size()>0){
+    //         if(left.size()>0){
+    //             temp=left.front();
+    //             left.pop();
+    //             result.push_back(temp->val);
+    //             if(temp->left!=NULL){
+    //                 left.push(temp->left);
+    //             }
+    //             if(temp->right!=NULL){
+    //                 right.push(temp->right);
+    //             }
+    //         }
+    //         else if(right.size()>0){
+    //             temp=right.top();
+    //             right.pop();
+    //             result.push_back(temp->val);
+    //             if(temp->left!=NULL){
+    //                 left.push(temp->left);
+    //             }
+    //             if(temp->right!=NULL){
+    //                 right.push(temp->right);
+    //             }
+    //         }
+    //     }
+    //     return result;
     }
 };
